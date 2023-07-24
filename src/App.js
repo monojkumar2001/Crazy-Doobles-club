ea6608e75767a3b4790d5be2c633c0fa09fc42ab
monoj <monojkumar57859893@gmail.com>
2023-07-24 17:27:08 +0600

    update

--- src/App.js
+++ src/App.js
@@ -18,10 +18,10 @@ function App() {
     <BrowserRouter>
     <Header/>
     <Routes>
-      <Route path="/Home" element={<HomePage/>} />
+      <Route path="/" element={<WhiteListPage/>} />
     </Routes>
     <Routes>
-      <Route path="/" element={<WhiteListPage/>} />
+      <Route path="/Home" element={<HomePage/>} />
     </Routes>
     <Routes>
       <Route path="/whitelist-2" element={<WhiteListPage2/>} />
